0304b6d1aba35a320439a704a4ab40ea8b6f8822
thuijskens <thomas_huijskens@hotmail.com>
2015-02-24 23:26:50 +0000

    Added original data

--- Hierarchical models/models.R
+++ Hierarchical models/models.R
@@ -45,24 +45,72 @@ schools$schdenom <- factor(schools$schdenom,
                            labels = c("Roman Catholic",
                                       "Non-denominational"))
 
+schools <- schools %>% filter(score != 0)
 
-# First we fit an intercept-only mixed effects model, to show that
+# First we fit a very basic mixed effects model, to show that
 # there is indeed a need to use a multilevel model (confirming the suspicion
 # that score depends on the school)
 
-schools_base <- lme(fixed = score ~ 1,
+schools_base <- lme(fixed = score ~ 1 + cohort90,
                     random = score ~ 1 | schoolid,
                     data = schools)
 
-# Need to do residual analysis
+schools_base_lm <- lm(score ~ 1 + cohort90, data = schools)
+
+anova(schools_base, schools_base_lm)
+# very significant
+
+# Need to do residual analysis:
+plot(schools_base, schoolid ~ resid(.), abline = 0, ylab = "School")
+# Very bad plot, only thing that is clear from this is that the variability is still rather large.
+
+qqnorm(schools_base, ~ resid(., type = "p"))
+# Residuals of the within group-errors seem normally distributed.
+
+
+
+
 
 # Now find better models:
 # 1. Interaction effect between schtype and schurban
 # 2. Interaction effect between sclass and schtype (?)
 # 3. cohort90 has an effect on the score
 
-schools_type <- lme(fixed = score ~ 1 + schtype*schurban,
+schools_type <- lme(fixed = score ~ 1 + schtype*schurban + cohort90 + sclass*schtype,
                     random = score ~ 1 | schoolid,
                     data = schools)
 
-# This is already a lot better
+schools_type_re <- lme(fixed = score ~ 1 + schtype*schurban + cohort90 + sclass*schtype,
+                       random = score ~ 1 + schtype | schoolid,
+                       data = schools)
+
+anova(schools_type, schools_type_re)
+# We reject the null hypothesis that schools_type is more appropriate) The random effects are significant
+
+# Adding cohort90 to the random effects part is not appropriate. The model then becomes a three-level model
+# since students are nested in cohorts and cohorts are nested in schools.
+
+# Adding sclass to the random effects part is also not appropriate as the random effects model between-school
+# variability and social class is a student-level variable.
+schools_cohort <- lme(fixed =  score ~ 1 + cohort90 + sclass,
+                      random = score ~ 1 | schoolid/cohort90,
+                      data = schools)
+
+schools_cohort2 <- lme(fixed =  score ~ 1 + cohort90 + sclass*schtype,
+                      random = score ~ 1 | schoolid/cohort90,
+                      data = schools)
+
+# Difference in log likelihood is small
+
+schools_cohort3 <- lme(fixed =  score ~ 1 + cohort90 + sclass + sclass*schurban,
+                       random = score ~ 1 | schoolid/cohort90,
+                       data = schools)
+
+# sclass*schurban is not significant at all
+
+schools_cohort4 <- lme(fixed =  score ~ 1 + cohort90 + sclass + schtype,
+                       random = score ~ 1 | schoolid/cohort90,
+                       data = schools)
+
+# almost everything is highly significant!
+
